41e793c84eb12d6e4c9d693b233a5e4154f85bf2
bmalamin <bmalamin940@gmail.com>
2023-01-15 22:15:40 +0600

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,34 @@
+//MIXITUP
+var mixer = mixitup('.grid-portfolio-container');
+ // Scrollify
+
+$(function() {
+    $.scrollify({
+        section : ".scrollify",
+        setHeights: false,
+        updateHash: false,
+        scrollSpeed: 1100,
+    });
+});
+ 
+//HUMBURGER MENU
+
+function openMenu() {
+    document.getElementById('navbar').style.height = "100%";
+}
+
+function closeMenu() {
+    document.getElementById('navbar').style.height = "0%";
+}
+
+// Smooth Scroll
+
+$(function(){
+    $('.menu-item a, .scroll-down a').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+        
+        return false;
+    });
+});
